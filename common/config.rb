b4b7e7f29b6e60b628a63937637b039086131775
Oliver Walker <olly@ventureweb.net>
2013-11-01 13:28:48 -0700

    update

---
+++ common/config.rb
@@ -0,0 +1,33 @@
+# Require any additional compass plugins here.
+require "toolkit"
+require "breakpoint"
+#require "singularitygs"
+require "sassy-buttons"
+require "color-schemer"
+require "modular-scale"
+require "stitch"
+require "animation"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets/css"
+sass_dir = "stylesheets/scss"
+images_dir = "assets/images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass stylesheets/scss scss && rm -rf sass && mv scss sass
---
+++ common/js/script.js
@@ -0,0 +1,156 @@
+/* --------------------------------------------------
+
+	scrollin parallax fx
+	
+-------------------------------------------------- */
+var ScrollFX = (function ($) {
+    var jk = {};
+    jk.config = {
+    	factor: 0.5,
+    	bg: 0,
+    	scrolling: true,
+    	winY: 0,
+    	winH: $(window).outerHeight(),
+    	winW: $(window).outerWidth()
+    };
+    
+    jk.init = function () {
+    	jk.parallax.init();
+    	jk.window();
+    };
+    jk.window = function () {
+    	var $window = $(window);
+    	$window.resize(function() {
+    		jk.config.winH = $(window).outerHeight();
+    		jk.config.winW = $(window).outerWidth();
+    	});
+    	
+    	$window.scroll(function() {
+    		jk.config.winY = $(this).scrollTop();
+    		setInterval(
+    			function(){
+    				jk.config.scrolling = true;
+    			}
+    		, 30);
+    		
+		});
+    	
+    };
+    jk.parallax = {
+    	config : {
+    	},    	
+    	init : function () {
+    		jk.parallax.loop();
+    	},
+    	animate: function () {
+        	jk.config.$backgrounds.each(function(index) {
+        		var y = Math.round(($(this).find(".para").offset().top - jk.config.winY) * jk.config.factor + (index+1));
+				$(this).find(".para").css({"-webkit-transform": "translateY(" + (y) + "px)"});
+				$(this).css({"background-position": "50% " + -(y/8) + "%"});
+			});
+        },
+    	loop : function () {
+    		var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    		
+    	    if (jk.config.scrolling) {
+    	    	jk.parallax.animate();
+        		jk.config.scrolling = false;
+        		
+        	}
+        	requestAnimationFrame(jk.parallax.loop);
+    	}
+    };
+    jk.inView = function (elem, percentage) {
+        var docViewTop = $(window).scrollTop(),
+            docViewBottom = docViewTop + $(window).height(),
+            elemTop = $(elem).offset().top,
+            elemBottom = elemTop + ($(elem).height() / 100 * percentage);
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop) || (docViewTop - elemTop >= 0));
+    };
+    return jk;
+})(jQuery);
+/* --------------------------------------------------
+
+	ui
+	
+-------------------------------------------------- */
+var UI = (function ($) {
+    var jk = {};
+    jk.config = {
+    	winW: $(window).outerWidth(),
+    	winH: $(window).outerHeight(),
+    	$layout_container: $('.set'),
+    	$viewport: $(".viewport"),
+    	$nvpMenu_toggle: $(".menu_toggle"),
+    };
+    jk.init = function () {
+    	jk.window();
+    	jk.viewport();
+    	//jk.layout();
+    	jk.ui.nvpMenu();
+
+    };
+    jk.viewport = function () {
+    	var h = jk.config.winH;    	
+    	
+    };
+    jk.layout = function () {
+    	var sizer = Math.floor(jk.config.$layout_container.outerWidth() / 4);
+		console.log(sizer);
+		jk.config.$layout_container.masonry({
+			columnWidth: sizer,
+			isFitWidth: true,
+			gutter: 0,
+			itemSelector: '.panel'
+		});
+    };
+    jk.window = function () {
+    	var $window = $(window);
+    	
+    	$window.resize(function () {
+    		jk.config.winH = $window.outerHeight();
+    		jk.viewport();
+    		//jk.layout();
+    	});
+    }
+  	jk.ui = {
+  		nvpMenu : function () {
+	  		var _viewport = jk.config.$viewport,
+	  			_toggle = jk.config.$nvpMenu_toggle;	  		
+	  		_toggle.on("click", this, function(e) {
+	  			e.preventDefault();
+	  			_viewport.toggleClass("open");
+	  		});
+	  		
+  		}
+
+  	};
+  	jk.scrollPage = function (i, o, d) {
+		var y = $(i).offset().top - o;
+  		$('html, body').animate({
+			scrollTop: y
+		}, {duration: d, queue: false});
+  	};
+    return jk;
+})(jQuery);
+
+/*
+	start it up
+*/
+// ################################################################################
+$(function () {
+
+	UI.init();
+	//ScrollFX.init();
+	// if video?
+	if ($("#vidBG").length) {
+		_V_("vidBG").ready(function() {
+    		var myPlayer = this;
+    		myPlayer.width(window);
+       		myPlayer.volume(0);
+			myPlayer.play();
+		});
+	}
+	
+	
+});
